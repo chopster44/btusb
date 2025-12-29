93d3877dcec7bcb7a5ebde8c0cc1b47d75a0c427
chopster44 <oscar@chopster44.com>
2025-12-29 18:41:28 +0000

    fixed the KERNEL_SEMVER for debian

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-KERNEL_SEMVER=$(uname -r | cut -d- -f1)
+KERNEL_SEMVER=$(uname -r | cut -d+ -f1)
 
 for i in $(echo "intel bcm rtl mtk" | sed 's/ /\n/g'); do
-  curl -O "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.h?h=v$KERNEL_SEMVER"
-	curl -O "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.c?h=v$KERNEL_SEMVER"
+        echo "updating bt$i.h, https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.h?h=v$KERNEL_SEMVER"
+        curl -O "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.h?h=v$KERNEL_SEMVER"
+	echo "updating bt$i.c, https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.c?h=v$KERNEL_SEMVER"
+        curl -O "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/bt$i.c?h=v$KERNEL_SEMVER"
 done
 
+echo "updating btusb.c, https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/btusb.c?h=v$KERNEL_SEMVER"
 curl -O "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/bluetooth/btusb.c?h=v$KERNEL_SEMVER"
